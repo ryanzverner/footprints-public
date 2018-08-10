a9aba0e0e55b92dfb39fe3ddf47b3f5ddf2c92d8
gratefulcheddar <timthomascode@icloud.com>
2018-08-10 12:46:13 -0500

    Playground: Testing data structure passed to reporting/index.html.rb

--- app/controllers/reporting_controller.rb
+++ app/controllers/reporting_controller.rb
@@ -6,13 +6,21 @@ class ReportingController < ApplicationController
   def index
     interactor = ReportingInteractor.new(session[:id_token])
     @reporting_data = interactor.fetch_projection_data(Date.today.month, Date.today.year)
-  rescue ReportingInteractor::AuthenticationError => e
-    error_message = "You are not authorized through warehouse to use this feature"
+    puts @reporting_data
+    @reporting_data = { "Aug 2018" => { "Software Craftsmen" => repo.craftsman.where("skill = 2", 0).count,
+                                              "UX Craftsmen" => repo.craftsman.where("skill = 1", 0).count,
+                                              "Software Residents" => 99,
+                                              "UX Residents" => 99,
+                                              "Finishing Software Residents" => 99,
+                                              "Finishing UX Residents" => 99,
+                                              "Student Apprentices" => 99 } }
+  # rescue ReportingInteractor::AuthenticationError => e
+  #   error_message = "You are not authorized through warehouse to use this feature"
 
-    Rails.logger.error(error_message)
-    Rails.logger.error(e.message)
-    Rails.logger.error(e.backtrace)
+  #   Rails.logger.error(error_message)
+  #   Rails.logger.error(e.message)
+  #   Rails.logger.error(e.backtrace)
 
-    redirect_to root_path, :flash => { :error => [error_message] }
+  #   redirect_to root_path, :flash => { :error => [error_message] }
   end
 end
