df008da993b24aa702bd4cbbd0b52c9ca99812c7
gratefulcheddar <timthomascode@icloud.com>
2018-08-23 13:27:14 -0500

    Fix remaining ApprenticesController tests

--- spec/controllers/apprentices_controller_spec.rb
+++ spec/controllers/apprentices_controller_spec.rb
@@ -46,17 +46,23 @@ describe ApprenticesController do
   end
 
   context "PUT #update" do
+  
     it "responds 302 after updating a resident" do
+      allow(Footprints::Repository.apprentice).to receive(:find).and_return(Apprentice.new)
+      allow_any_instance_of(Apprentice).to receive(:save!)
       put :update, :id => "208", :apprentice => {:end_date => Date.tomorrow}
       expect(response.status).to eq(302)
     end
 
     it "throws an error when no value is provided" do
+      allow(Footprints::Repository.apprentice).to receive(:find).and_return(Apprentice.new)
       put :update, :id => "208", :apprentice => {:end_date => ""}
       expect(flash[:error]).to eq ["Please provide a valid date"]
     end
 
-    it "redirects to the resident show page" do
+    it "redirects to the apprentice index page" do
+      allow(Footprints::Repository.apprentice).to receive(:find).and_return(Apprentice.new)
+      allow_any_instance_of(Apprentice).to receive(:save!)
       put :update, :id => "208", :apprentice => {:end_date => Date.tomorrow}
       expect(response).to redirect_to("/apprentices/")
     end
